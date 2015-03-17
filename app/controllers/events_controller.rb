f5a71d4ad0176fa9bb0f3f7435c967cf77e6508c
Remina Greenfield <reminagreenfield@gmail.com>
2015-03-17 19:39:32 -0400

    adds chat ip

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -62,13 +62,19 @@ class EventsController < ApplicationController
 
   end
 
-  def foursquare
-    baseURL = "https://api.foursquare.com/v2/venues/explore"
-    clientID = "?client_id=CSXMPXYF20VXWMX4Z0BTKHVT5VGRKA1E3ZAPKCE04ELOMX3W"
-    clientSecret = "&client_secret=LQ2UIGEDAP0O5CFMQEMEYEM1KORYH4ISVPXLRSHGYNU1LMOZ"
+  # def yelp
+  #   binding.pry
+  #   coordinates = params[:coordinates]
+  #   api(coordinates)
+  # end
 
-    render text: Net::HTTP.get baseURL + clientID + clientSecret + '&' + params[:foursquare].to_param
-  end
+  # def foursquare
+  #   baseURL = "https://api.foursquare.com/v2/venues/explore"
+  #   clientID = "?client_id=CSXMPXYF20VXWMX4Z0BTKHVT5VGRKA1E3ZAPKCE04ELOMX3W"
+  #   clientSecret = "&client_secret=LQ2UIGEDAP0O5CFMQEMEYEM1KORYH4ISVPXLRSHGYNU1LMOZ"
+
+  #   render text: Net::HTTP.get baseURL + clientID + clientSecret + '&' + params[:foursquare].to_param
+  # end
 
   # POST /events
   # POST /events.json
